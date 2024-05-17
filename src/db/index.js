13d1efbd756a2bd6ee60cb469cb3241e9b5fc5fc
vikash-ftw <vikashbatham97@gmail.com>
2024-05-17 14:01:40 +0530

    replaced async await with promises

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
+// async approach
+/*
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
@@ -13,6 +15,25 @@ const connectDB = async () => {
     console.log("MongoDB connection error: ", error);
     process.exit(1);
   }
+};
+*/
+
+// promise (resolve-reject) approach
+// mongoose.connect return a promise - so we do not need to create a promise
+// we just use .then and .catch to handle the response
+// create a function - inside it handle the promise and then export the function
+const connectDB = () => {
+  mongoose
+    .connect(`${process.env.MONGODB_URL}/${DB_NAME}`) // .connect() returns promise
+    .then((connectionInstance) => {
+      console.log(
+        `\nMongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
+      );
+    })
+    .catch((error) => {
+      console.log("MongoDB connection error: ", error);
+      process.exit(1);
+    });
 };
 
 export default connectDB;
