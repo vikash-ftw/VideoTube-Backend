99e514353fa38848fa096d5fcae2ef5586646b86
vikash-ftw <vikashbatham97@gmail.com>
2024-05-18 18:28:39 +0530

    fixed some imports and renamed files

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import cors from cors
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
@@ -7,26 +7,31 @@ const app = express();
 // configure the cors
 // app.use(cors()) // normal configured
 // prod based configuration
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN, // to manage req origin source ex- origin: 'http://example.com'
-}))
+  })
+);
 
 // configure the data type we receive in request
-app.use(express.json({
+app.use(
+  express.json({
     limit: "25kb", // limit data size
-}))
+  })
+);
 
 // configure the url encoder for url data like %20 and +
-app.use(express.urlencoded({
+app.use(
+  express.urlencoded({
     extended: true, // allow objects in objects - nested objects - not used mostly
     limit: "25kb",
-}))
+  })
+);
 
 // configure the file local storage on server - local asset storage on public folder
-app.use(express.static("public")) // here public is the folder name
+app.use(express.static("public")); // here public is the folder name
 
 // configure the cookie handling
-app.use(cookieParser())
-
+app.use(cookieParser());
 
 export { app };
