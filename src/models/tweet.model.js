80ecd02358b31d9af1eaab078f6bd5146862f5c9
vikash-ftw <vikashbatham97@gmail.com>
2024-05-24 10:48:41 +0530

    added comment, like, playlist and tweet models

---
+++ src/models/tweet.model.js
@@ -0,0 +1,18 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const tweetSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Tweet = mongoose.model("Tweet", tweetSchema);
